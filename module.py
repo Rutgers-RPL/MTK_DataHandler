301b1ade45d2e71d0119d096f79f966ef909537f
Tyler Abbassi <tfa21@scarletmail.rutgers.edu>
2024-11-18 11:27:24 -0500

    staging additions

--- module.py
+++ module.py
@@ -11,6 +11,12 @@ class FlightDataHandler:
         self.packet_data = None
         self.state_data = None
         self.config = None
+        self.state_data_dict = None
+
+        self.state_column_to_index = {'magic':0, 'state_flags':1, 'arming_time':2, 
+                                      'apogee_time':3, 'drogue_time': 4, 'main_time': 5, 
+                                      'sus_time':6}
+
         self.column_to_index = {'magic':0, 'status':1, 'time_us':2, 'main_voltage_v':3, 'pyro_voltage_v':4, 'numSatellites':5, 'gpsFixType':6, 'latitude_degrees':7, 
     'longitude_degrees':8, 'gps_hMSL_m':9, 'barometer_hMSL_m':10, 'temperature_c':11, 'acceleration_x_mss':12, 'acceleration_y_mss':13, 'acceleration_z_mss':14, 
     'angular_velocity_x_rads':15, 'angular_velocity_y_rads':16, 'angular_velocity_z_rads':17, 'gauss_x':18, 'gauss_y':19, 'gauss_z':20, 
@@ -49,3 +55,54 @@ class FlightDataHandler:
     def display_metadata(self):
         print(f"Launch Date: {self.launch_date}")
         print(f"Board Version: {self.board_version}")
+
+    def find_stages(self):
+
+        #populates state_data_dict with column_name value pairings
+
+        if self.state_data is None:
+            print("No state data loaded.")
+            return
+
+        self.state_data_dict = {}
+
+        for column_name, index in self.state_column_to_index.items():
+            self.state_data_dict[column_name] = self.state_data[1, index]
+
+    
+    def plot_column_with_stages(self, column_name, title = None, start = None, end = None):
+        if start is None:
+            start = 0
+        if self.data is None:
+            print("No data loaded.")
+            return
+        if column_name not in self.column_to_index:
+            print(f"Column: '{column_name}' not found in data.")
+            return
+        index = self.column_to_index[column_name]
+        if end is None:
+            end = self.packet_data[index].shape[0]
+
+        colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', '#FF5733']
+
+        if not self.state_data_dict:
+            print("No state data loaded.")
+            return
+
+        for i, (stage_name, stage_time) in enumerate(self.state_data_dict.items()):
+            color = colors[i % len(colors)]  
+            plt.axvline(x=stage_time, color=color, linestyle='--', label=stage_name)
+        
+
+        self.packet_data[index].plot(kind='line', title=title or f"{column_name} over Time")
+        plt.xlabel("Index")
+        plt.ylabel(column_name)
+        plt.show()
+
+    def display_metadata(self):
+        print(f"Launch Date: {self.launch_date}")
+        print(f"Board Version: {self.board_version}")
+    
+
+        
+        
