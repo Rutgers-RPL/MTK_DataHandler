7fae9a50fdfdd6a5c0fffb0dcdee28e3f1ebf6c4
Tyler Abbassi <tfa21@scarletmail.rutgers.edu>
2024-11-20 22:53:00 -0500

    Added staging changes in the correct file

--- MTK_DataHandler.py
+++ MTK_DataHandler.py
@@ -11,17 +11,20 @@ class FlightDataHandler:
         self.board_version = board_version
         self.packet_data = None
         self.state_data = None
-        self.state_data_dict = {}
+
+        self.state_activations = {}
+
+        self.state_column_to_index = {'magic':0, 'state_flags':1, 'arming_time':2, 
+                                      'apogee_time':3, 'drogue_time': 4, 'main_time': 5, 
+                                      'sus_time':6}
+        
 
         self.config = None
+
         self.column_to_index = {'magic':0, 'status':1, 'time_us':2, 'main_voltage_v':3, 'pyro_voltage_v':4, 'numSatellites':5, 'gpsFixType':6, 'latitude_degrees':7, 
     'longitude_degrees':8, 'gps_hMSL_m':9, 'barometer_hMSL_m':10, 'temperature_c':11, 'acceleration_x_mss':12, 'acceleration_y_mss':13, 'acceleration_z_mss':14, 
     'angular_velocity_x_rads':15, 'angular_velocity_y_rads':16, 'angular_velocity_z_rads':17, 'gauss_x':18, 'gauss_y':19, 'gauss_z':20, 
     'kf_acceleration_mss':21, 'kf_velocity_ms':22, 'kf_position_m':23, 'w':24, 'x':25, 'y':26, 'z':27, 'checksum':28}
-        
-        self.state_column_to_index = {'magic':0, 'state_flags':1, 'arming_time':2, 
-                                      'apogee_time':3, 'drogue_time': 4, 'main_time': 5, 
-                                      'sus_time':6}
 
     def update_column_to_index(self,updated):
         self.column_to_index = updated
@@ -128,14 +131,20 @@ class FlightDataHandler:
 
     def find_stages(self):
 
-        #populates state_data_dict with column_name value pairings
+        #populates state_activations with column_name value pairings
 
         if self.state_data is None:
             print("No state data loaded.")
             return
 
-        for column_name, index in self.state_column_to_index.items():
-            self.state_data_dict[column_name] = self.state_data[1, index]
+        for stage, col in self.state_column_to_index.items():
+            column = self.state_data[:, col]
+            activation = np.where(np.diff(column)==1)[0]
+            if len(activation) > 0:
+                self.state_activations[stage] = activation[0] + 1  
+            else:
+                self.state_activations[stage] = None
+                
 
     
     def plot_column_with_states(self, column_name, title=None, start=None, end=None):
@@ -161,14 +170,14 @@ class FlightDataHandler:
 
         colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', '#FF5733']
 
-        if not self.state_data_dict:
-            print("No state data loaded.")
+        if not self.state_activations:
+            print("No state data loaded/parsed.")
             return
 
-        for i, (stage_name, stage_time) in enumerate(self.state_data_dict.items()):
-            color = colors[i % len(colors)]
-            if (start < stage_time and end > stage_time):  
-                plt.axvline(x=stage_time, color=color, linestyle='--', label=stage_name) 
+        for i, (stage_name, stage_time) in enumerate(self.state_activations.items()):
+            if start <= stage_time < end: 
+                color = colors[i % len(colors)]  
+                plt.axvline(x=stage_time, color=color, linestyle='--', label=stage_name)
 
 
         plt.plot(range(start, end), column_data, label=column_name)
